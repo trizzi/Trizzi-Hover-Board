cc9a9045c00a8047de05081aad963eeae4c888a0
kryptkode <paulakhamiogu@gmail.com>
2021-06-20 06:13:21 +0100

    modified script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.getElementById('container');
-const color = ['#e743c', '#8e44ad', '#3498db', '#e67e22'];
+const colors = ['#e743c', '#8e44ad', '#3498db', '#e67e22'];
 const SQUARES = 500;
 
 for (let i = 0; i < SQUARES; i++) {
